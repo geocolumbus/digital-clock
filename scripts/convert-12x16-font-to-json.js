b0e267994bdbe68ba82c8a043738eb34c722a7eb
George Campbell <geocolumbus@gmail.com>
2020-02-03 19:46:42 -0500

    Add double sized font

---
+++ scripts/convert-12x16-font-to-json.js
@@ -0,0 +1,35 @@
+"use strict"
+
+const fs = require("fs")
+
+const file = "font-12x16"
+const source = `../fonts/raster-fonts/${file}.c`
+const sink = `../src/json/${file}.json`
+
+const sourceFileLines = fs.readFileSync(source, {encoding: "UTF-8"}).toString().split("\n")
+
+let asciiCode = null
+let lineCounter = 0
+let font = []
+sourceFileLines.forEach(line => {
+    if (line.includes("* code=")) {
+        const asciiMatch = line.match(/code=([0-9]*)\s?,/)
+        if (asciiMatch && asciiMatch.length > 0) {
+            asciiCode = asciiMatch[1]
+            lineCounter = 0
+            font[asciiCode] = []
+        }
+    }
+    if (lineCounter === 18) {
+        asciiCode = null
+        lineCounter = 0
+    }
+    if (asciiCode && lineCounter > 1) {
+        font[asciiCode].push(line.match(/\*\s([01]*)\s\*/)[1].replace(/0/g," "))
+    }
+    if (asciiCode) {
+        lineCounter = lineCounter + 1
+    }
+})
+fs.writeFileSync(sink,JSON.stringify(font,null,2))
+
